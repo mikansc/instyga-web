2c2763daad1af2f794b7a78037df58ca3972dbea
Michael Nascimento <michael.nsc@outlook.com>
2023-07-24 17:55:45 -0300

    componente titulo

---
+++ components/Titulo/Titulo.tsx
@@ -0,0 +1,3 @@
+export const Titulo = ({ children }: { children: React.ReactNode }) => {
+  return <h2 className="text-4xl font-bold text-gray-900 mb-6">{children}</h2>;
+};
---
+++ components/Titulo/index.ts
@@ -0,0 +1,1 @@
+export { Titulo } from "./Titulo";
