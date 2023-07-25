4c13b86aef6fca68c715830f7707c9733d5c2b7d
Michael Nascimento <michael.nsc@outlook.com>
2023-07-25 08:09:58 -0300

    configura env

---
+++ constants/environment.ts
@@ -0,0 +1,6 @@
+export const ENV = {
+  TRILHA_API_URL:
+    process.env.TRILHA_API_URL ?? "http://localhost:81/api/trilhas",
+  AVALIACAO_API_URL:
+    process.env.AVALIACAO_API_URL ?? "http://localhost:82/api/avaliacoes",
+};
